90cac71391226f2300e4454292d6867425dc3df2
Olivier <olivier18102@gmail.com>
2024-02-26 13:42:52 -0500

    Update

---
+++ somme_de_question_pairs/exec.py
@@ -0,0 +1,13 @@
+# Demander à l'utilisateur d'entrer un nombre entier positif
+nombre = int(input("Entrez un nombre entier positif : "))
+
+# +VISIBLE
+somme_pairs = 0
+
+# -TODO
+for i in range():  
+# +TODO
+
+
+print("La somme des nombres pairs jusqu'à", nombre, "est :", somme_pairs)
+# -VISIBLE
